04763096c6d9dbbb07666cd03e9c49cada7eb9c5
Leandro Gargantini <leandrofgargantini@gmail.com>
2024-09-14 20:21:31 -0300

    @lgargantini: remove unused suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import React, { Suspense } from "react";
+import React from "react";
 import "./App.css";
 import { Breeds } from "./Breeds";
 import Header from "./Header";
-import { Logo } from "./Header/Logo";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Suspense fallback={
-                <Logo />
-            }>
-                <Breeds />
-            </Suspense>
+            <Breeds />
         </div>
     );
 }
